32669c3d9910788752aadbe2814cf5ab58933073
brent <brent@n3f.com>
2025-08-18 13:59:54 -0700

    Add a deployment script.
    (see https://n3f.com/deploying-laravel-application-to-nearlyfreespeech-net/)

---
+++ scripts/adjust-nearlyfreespeech-permissions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directories=(
+    "database"
+    "storage"
+    "bootstrap/cache"
+)
+
+for directory in "${directories[@]}"; do
+    chown -R web "$directory"
+    chmod -R 755 "$directory"
+done
